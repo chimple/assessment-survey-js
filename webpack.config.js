5f0bce8fe241dfa0d4ae5ce55b75ff63076443ac
Skanda <skanda@sutara.org>
2025-06-05 10:50:11 +0530

    Fixed the Webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/App.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
         exclude: /node_modules/,
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'manifest.json', to: '.' },
+        { from: 'data', to: 'data' },
+        { from: 'audio', to: 'audio' },
+        { from: 'css', to: 'css' },
+        { from: 'animation', to: 'animation' },
+        { from: 'img', to: 'img' },
+      ],
+    }),
+  ],
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
   },
   output: {
     filename: 'bundle.js',
